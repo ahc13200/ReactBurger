b3a03a61edc5938d4d24336c8f2a7cfc3518e7f6
ahc13200 <ahc13200@gmail.com>
2024-02-22 18:58:07 -0500

    ajustes
    Signed-off-by: ahc13200 <ahc13200@gmail.com>

--- src/components/Header/ItemHeader.jsx
+++ src/components/Header/ItemHeader.jsx
@@ -1,6 +1,6 @@
 export default function ItemHeader({children, isMobile}) {
 	let classNameItem =
-		'flex justify-center items-center rounded-xl p3 hover:text-yellow-400 hover:bg-stone-300/20 hover:cursor-pointer transition-all duration-400';
+		'flex justify-center items-center rounded-xl p2 hover:text-yellow-400 hover:bg-stone-300/20 hover:cursor-pointer transition-all duration-400';
 
 	if (isMobile) classNameItem = 'flex justify-center items-center rounded-full p3 bg-white:10 hover:text-yellow-400';
 
