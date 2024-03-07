e9ba435d4d54f9f64b147253f8db8a56f41b8297
ahc13200 <ahc13200@gmail.com>
2024-03-06 19:00:38 -0500

    composable
    Signed-off-by: ahc13200 <ahc13200@gmail.com>

--- src/composables/useListFetch.js
+++ src/composables/useListFetch.js
@@ -4,14 +4,11 @@ const baseURL = 'http://localhost:3000/';
 
 export async function listFetch(endpoint) {
 	const response = await axios.get(`${baseURL}` + endpoint);
-
 	let result = response.status === 200 ? response.data : response.error;
-
 	return result;
 }
 
-export async function create(endpoint, data) {
+export async function createFetch(endpoint, data) {
 	const response = await axios.post(`${baseURL}` + endpoint, data);
-
 	return response.status;
 }
